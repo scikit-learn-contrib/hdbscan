82877e0da2f2fc5f61ce6ff2796fc089aff21fa8
Leland McInnes <leland.mcinnes@gmail.com>
2015-07-21 15:33:24 -0400

    Add extra setup fields

--- setup.py
+++ setup.py
@@ -14,10 +14,39 @@ _hdbscan_tree = Extension('hdbscan/_hdbscan_tree',
 _hdbscan_linkage = Extension('hdbscan/_hdbscan_linkage',
                              sources=['hdbscan/_hdbscan_linkage.pyx'])
 
+def readme():
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
 configuration = {
     'name' : 'hdbscan',
+    'version' : '0.1',
+    'description' : 'Clustering based on density with variable density clusters',
+    'long_description' : readme(),
+    'classifiers' : [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    'keywords' : 'cluster clustering density hierarchical',
+    'url' : 'http://github.com/lmcinnes/hdbscan',
+    'maintainer' : 'Leland McInnes',
+    'maintainer_email' : 'leland.mcinnes@gmail.com',
+    'license' : 'BSD',
     'packages' : ['hdbscan'],
-    'install_requires' : ['cython >= 0.17'],
+    'install_requires' : ['sklearn>=0.16',
+                          'cython >= 0.17'],
     'ext_modules' : [_hdbscan_tree, _hdbscan_linkage],
     'cmdclass' : {'build_ext' : build_ext}
     }
@@ -25,7 +54,7 @@ configuration = {
 if not HAVE_CYTHON:
     _hdbscan_tree.sources[0] = '_hdbscan_tree.c'
     _hdbscan_linkage.sources[0] = '_hdbscan_linkage.c'
-    configuration.pop('install_requires')
+    configuration['install_requires'] = ['sklearn>=0.16']
 
 setup(**configuration)
  
