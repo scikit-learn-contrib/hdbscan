f218633159a03459c6bd80b8254fc9e0872b6c85
Leland McInnes <leland.mcinnes@gmail.com>
2015-09-07 18:54:29 -0400

    Move mutual reachability to a cython file

--- hdbscan/hdbscan_.py
+++ hdbscan/hdbscan_.py
@@ -22,71 +22,19 @@ except ImportError:
     from sklearn.utils import check_arrays
     check_array = check_arrays
 
-from ._hdbscan_linkage import single_linkage, mst_linkage_core, label
+from ._hdbscan_linkage import single_linkage, mst_linkage_core, mst_linkage_core_pdist, label
 from ._hdbscan_tree import (get_points,
                             condense_tree, 
                             compute_stability, 
                             get_clusters)
+from ._hdbscan_reachability import kdtree_pdist_mutual_reachability, kdtree_mutual_reachability, mutual_reachability
 from .plots import CondensedTree, SingleLinkageTree, MinimumSpanningTree
-  
-def mutual_reachability(distance_matrix, min_points=5):
-    """Compute the weighted adjacency matrix of the mutual reachability
-    graph of a distance matrix.
-    
-    Parameters
-    ----------
-    distance_matrix : array [n_samples, n_samples]
-        Array of distances between samples.
-        
-    min_points : int optional
-        The number of points in a neighbourhood for a point to be considered
-        a core point. (defaults to 5)
 
-    Returns
-    -------
-    mututal_reachability: array [n_samples, n_samples]
-        Weighted adjacency matrix of the mutual reachability graph.
-        
-    References
-    ----------
-    R. Campello, D. Moulavi, and J. Sander, "Density-Based Clustering Based on
-    Hierarchical Density Estimates"
-    In: Advances in Knowledge Discovery and Data Mining, Springer, pp 160-172.
-    2013
-    """
-    dim = distance_matrix.shape[0]
-    min_points = min(dim - 1, min_points)
-    try:
-        core_distances = np.partition(distance_matrix, 
-                                      min_points, 
-                                      axis=0)[min_points]
-    except AttributeError:
-        core_distances = np.sort(distance_matrix,
-                                 axis=0)[min_points]        
-                                  
-    stage1 = np.where(core_distances > distance_matrix, 
-                      core_distances, distance_matrix)
-    result = np.where(core_distances > stage1.T,
-                      core_distances.T, stage1.T).T
-    return result.astype(np.double)
-
-def kdtree_mutual_reachability(X, distance_matrix, metric, p=2, min_points=5):
-    dim = distance_matrix.shape[0]
-    min_points = min(dim - 1, min_points)
-
-    if metric == 'minkowski':
-        tree = KDTree(X, metric=metric, p=p)
-    else:
-        tree = KDTree(X, metric=metric)
-
-    core_distances = tree.query(X, k=min_points)[0][:,-1]
-
-    stage1 = np.where(core_distances > distance_matrix, 
-                      core_distances, distance_matrix)
-    result = np.where(core_distances > stage1.T,
-                      core_distances.T, stage1.T).T
-    return result.astype(np.double)
-
+try:
+    from fastcluster import single
+    HAVE_FASTCLUSTER = True
+except ImportError:
+    HAVE_FASTCLUSTER = False
 
 def hdbscan(X, min_cluster_size=5, min_samples=None, metric='minkowski', p=2):
     """Perform HDBSCAN clustering from a vector array or distance matrix.
