ae9b50b72d69a06c47cff0bc78d58346399b65d3
Leland McInnes <leland.mcinnes@gmail.com>
2017-02-16 13:21:57 -0500

    Add prediction functions to imports in init

--- hdbscan/__init__.py
+++ hdbscan/__init__.py
@@ -1,2 +1,3 @@
 from .hdbscan_ import HDBSCAN, hdbscan
 from .robust_single_linkage_ import RobustSingleLinkage, robust_single_linkage
+from .prediction import approximate_predict, membership_vector, all_points_membership_vectors
