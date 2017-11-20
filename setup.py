9a7549f7e57b9d12456c94f864c8a75e354e672b
Nathaniel Saul <nat@saulgill.com>
2017-11-20 11:40:09 -0800

    Does not need numpy installed before installing hdbscan

--- setup.py
+++ setup.py
@@ -10,26 +10,34 @@ except ImportError as e:
     from setuptools.command.build_ext import build_ext
     HAVE_CYTHON = False
 
-import numpy
+
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
+
 
 _hdbscan_tree = Extension('hdbscan._hdbscan_tree',
-                          sources=['hdbscan/_hdbscan_tree.pyx'],
-                          include_dirs=[numpy.get_include()])
+                          sources=['hdbscan/_hdbscan_tree.pyx'])
 _hdbscan_linkage = Extension('hdbscan._hdbscan_linkage',
-                             sources=['hdbscan/_hdbscan_linkage.pyx'],
-                             include_dirs=['hdbscan', numpy.get_include()])
+                             sources=['hdbscan/_hdbscan_linkage.pyx'])
 _hdbscan_boruvka = Extension('hdbscan._hdbscan_boruvka',
-                             sources=['hdbscan/_hdbscan_boruvka.pyx'],
-                             include_dirs=['hdbscan', numpy.get_include()])
+                             sources=['hdbscan/_hdbscan_boruvka.pyx'])
 _hdbscan_reachability = Extension('hdbscan._hdbscan_reachability',
-                                  sources=['hdbscan/_hdbscan_reachability.pyx'],
-                                  include_dirs=[numpy.get_include()])
+                                  sources=['hdbscan/_hdbscan_reachability.pyx'])
 _prediction_utils = Extension('hdbscan._prediction_utils',
-                              sources=['hdbscan/_prediction_utils.pyx'],
-                              include_dirs=[numpy.get_include()])
+                              sources=['hdbscan/_prediction_utils.pyx'])
 dist_metrics = Extension('hdbscan.dist_metrics',
-                         sources=['hdbscan/dist_metrics.pyx'],
-                         include_dirs=[numpy.get_include()])
+                         sources=['hdbscan/dist_metrics.pyx'])
+
 
 def readme():
     with open('README.rst') as readme_file:
@@ -62,7 +70,8 @@ configuration = {
     'maintainer_email' : 'leland.mcinnes@gmail.com',
     'license' : 'BSD',
     'packages' : ['hdbscan'],
-    'install_requires' : ['scikit-learn>=0.16',
+    'install_requires' : ['numpy',
+                          'scikit-learn>=0.16',
                           'cython >= 0.17'],
     'ext_modules' : [_hdbscan_tree,
                      _hdbscan_linkage,
@@ -70,7 +79,7 @@ configuration = {
                      _hdbscan_reachability,
                      _prediction_utils,
                      dist_metrics],
-    'cmdclass' : {'build_ext' : build_ext},
+    'cmdclass' : {'build_ext' : CustomBuildExtCommand},
     'test_suite' : 'nose.collector',
     'tests_require' : ['nose'],
     'data_files' : ('hdbscan/dist_metrics.pxd',)
@@ -83,7 +92,6 @@ if not HAVE_CYTHON:
     _hdbscan_reachability.sources[0] = 'hdbscan/_hdbscan_reachability.c'
     _prediction_utils.sources[0] = 'hdbscan/_prediction_utils.c'
     dist_metrics.sources[0] = 'hdbscan/dist_metrics.c'
-    configuration['install_requires'] = ['scikit-learn>=0.16']
+    configuration['install_requires'] = ['numpy', 'scikit-learn>=0.16']
 
 setup(**configuration)
- 
