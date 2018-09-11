d580ccb67983b0e9829d4e67397be27bcd86759f
Leland McInnes <leland.mcinnes@gmail.com>
2018-09-11 10:40:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def requirements():
 
 configuration = {
     'name': 'hdbscan',
-    'version': '0.8.17',
+    'version': '0.8.18',
     'description': 'Clustering based on density with variable density clusters',
     'long_description': readme(),
     'classifiers': [
