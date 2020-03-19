5ca8b8be779c1f8d20a23409ab4613968c166f1a
gclen <graham.clendenning@uoit.net>
2020-03-19 12:35:55 -0400

    Release fix for #347

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def requirements():
 
 configuration = {
     'name': 'hdbscan',
-    'version': '0.8.25',
+    'version': '0.8.26',
     'description': 'Clustering based on density with variable density clusters',
     'long_description': readme(),
     'classifiers': [
