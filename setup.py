98928d0c095715edc9584e7989bd8559673bc2f0
Leland McInnes <leland.mcinnes@gmail.com>
2023-11-20 14:03:21 -0500

    Update setup.py; try a release candidate for wheel building

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def requirements():
 
 configuration = {
     'name': 'hdbscan',
-    'version': '0.8.33',
+    'version': '0.8.34-rc1',
     'description': 'Clustering based on density with variable density clusters',
     'long_description': readme(),
     'classifiers': [
