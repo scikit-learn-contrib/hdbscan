59316927eb93eb5ed04c54fd55c9d125bf106a77
Leland McInnes <leland.mcinnes@gmail.com>
2024-08-05 17:06:55 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def requirements():
 
 configuration = {
     'name': 'hdbscan',
-    'version': '0.8.37',
+    'version': '0.8.38',
     'description': 'Clustering based on density with variable density clusters',
     'long_description': readme(),
     'classifiers': [
